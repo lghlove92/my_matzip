25bdd04472ef408a227a12e3f6850b0cff00addf
hyho942 <hyunholee90@gmail.com>
2019-03-27 16:11:33 +0900

    ADD: address link, icon(css)

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,13 @@
 /***************************************************
  * 다음 지도 API 관련 로직
  */
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+var mapContainer = document.getElementById("map"), // 지도를 표시할 div
     mapOption = {
         center: new daum.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
         level: 7 // 지도의 확대 레벨
     };
 
-// 지도를 생성    
+// 지도를 생성
 var map = new daum.maps.Map(mapContainer, mapOption);
 
 // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
@@ -40,7 +40,6 @@ var infowindow = new daum.maps.InfoWindow({ zIndex: 1 });
  * @param {Number} longitude 경도
  */
 function my_location_find(latitude, longitude) {
-
     var latlng = new daum.maps.LatLng(latitude, longitude);
     searchAddrFromCoords(latlng, function (result, status) {
         if (status === daum.maps.services.Status.OK) {
@@ -61,13 +60,16 @@ function my_location_find(latitude, longitude) {
 
 // 키워드 검색을 요청하는 함수
 function searchPlaces() {
-    var keyword = document.getElementById('keyword').value;
-    if (!keyword.replace(/^\s+|\s+$/g, '')) {
-        alert('키워드를 입력해주세요!');
+    var keyword = document.getElementById("keyword").value;
+    var addrData = $(".form_location").val();
+    console.log(addrData);
+    if (!keyword.replace(/^\s+|\s+$/g, "")) {
+        alert("키워드를 입력해주세요!");
         return false;
     }
-    // 장소검색 객체를 통해 키워드로 장소검색을 요청
-    ps.keywordSearch(keyword, placesSearchCB);
+    // 장소검색 객체를 통해 키워드로 장소검색을 요청 (검색 결과 값 넣어서 같이 검색)
+    ps.keywordSearch(addrData + " " + keyword, placesSearchCB);
+    3;
 }
 
 // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
@@ -80,21 +82,21 @@ function placesSearchCB(data, status, pagination) {
         // 페이지 번호를 표출합니다
         displayPagination(pagination);
     } else if (status === daum.maps.services.Status.ZERO_RESULT) {
-        alert('검색 결과가 존재하지 않습니다.');
+        alert("검색 결과가 존재하지 않습니다.");
         return;
     } else if (status === daum.maps.services.Status.ERROR) {
-        alert('검색 결과 중 오류가 발생했습니다.');
+        alert("검색 결과 중 오류가 발생했습니다.");
         return;
     }
 }
 
 // 검색 결과 목록과 마커를 표출하는 함수입니다
 function displayPlaces(places) {
-    var listEl = document.getElementById('placesList'),
-        menuEl = document.getElementById('menu_wrap'),
+    var listEl = document.getElementById("placesList"),
+        menuEl = document.getElementById("menu_wrap"),
         fragment = document.createDocumentFragment(),
         bounds = new daum.maps.LatLngBounds(),
-        listStr = '';
+        listStr = "";
 
     // 검색 결과 목록에 추가된 항목들을 제거합니다
     removeAllChildNods(listEl);
@@ -114,16 +116,16 @@ function displayPlaces(places) {
         // 해당 장소에 인포윈도우에 장소명을 표시합니다
         // mouseout 했을 때는 인포윈도우를 닫습니다
         (function (marker, place) {
-            daum.maps.event.addListener(marker, 'mouseover', function () {
+            daum.maps.event.addListener(marker, "mouseover", function () {
                 displayInfowindow(marker, place.place_name);
             });
-            daum.maps.event.addListener(marker, 'mouseout', function () {
+            daum.maps.event.addListener(marker, "mouseout", function () {
                 infowindow.close();
             });
-            daum.maps.event.addListener(marker, 'click', function () {
-                var query = '?';
+            daum.maps.event.addListener(marker, "click", function () {
+                var query = "?";
                 for (var key in place) {
-                    query = query + key + '=' + place[key] + '&';
+                    query = query + key + "=" + place[key] + "&";
                 }
                 location.href = "detail.html" + query;
             });
@@ -154,44 +156,57 @@ function searchAddrFromCoords(coords, callback) {
 
 // 검색결과 항목을 Element로 반환하는 함수입니다
 function getListItem(index, places) {
-    var el = document.createElement('li'),
+    var el = document.createElement("li"),
         itemStr =
-            '<span class="markerbg marker_' + (index + 1) + '"></span>' +
+            '<span class="markerbg marker_' +
+            (index + 1) +
+            '"></span>' +
             '<div class="info">' +
-            '<h5>' + places.place_name + '</h5>';
+            "<h5>" +
+            places.place_name +
+            "</h5>";
 
     if (places.road_address_name) {
-        itemStr += '<span>' + places.road_address_name + '</span>' +
-            '<span class="jibun gray">' + places.address_name + '</span>';
+        itemStr +=
+            "<span>" +
+            places.road_address_name +
+            "</span>" +
+            '<span class="jibun gray">' +
+            places.address_name +
+            "</span>";
     } else {
-        itemStr += '<span>' + places.address_name + '</span>';
+        itemStr += "<span>" + places.address_name + "</span>";
     }
 
-    itemStr += '<span class="tel">' + places.phone + '</span>' +
-        '</div>';
+    itemStr += '<span class="tel">' + places.phone + "</span>" + "</div>";
     el.innerHTML = itemStr;
-    el.className = 'item';
+    el.className = "item";
 
     return el;
 }
 
 // 마커를 생성하고 지도 위에 마커를 표시하는 함수입니다
 function addMarker(position, idx, title) {
-    var imageSrc = 'http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png', // 마커 이미지 url, 스프라이트 이미지를 씁니다
-        imageSize = new daum.maps.Size(36, 37),  // 마커 이미지의 크기
+    var imageSrc =
+        "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png", // 마커 이미지 url, 스프라이트 이미지를 씁니다
+        imageSize = new daum.maps.Size(36, 37), // 마커 이미지의 크기
         imgOptions = {
             spriteSize: new daum.maps.Size(36, 691), // 스프라이트 이미지의 크기
-            spriteOrigin: new daum.maps.Point(0, (idx * 46) + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+            spriteOrigin: new daum.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
             offset: new daum.maps.Point(13, 37) // 마커 좌표에 일치시킬 이미지 내에서의 좌표
         },
-        markerImage = new daum.maps.MarkerImage(imageSrc, imageSize, imgOptions),
+        markerImage = new daum.maps.MarkerImage(
+            imageSrc,
+            imageSize,
+            imgOptions
+        ),
         marker = new daum.maps.Marker({
             position: position, // 마커의 위치
             image: markerImage
         });
 
     marker.setMap(map); // 지도 위에 마커를 표출합니다
-    markers.push(marker);  // 배열에 생성된 마커를 추가합니다
+    markers.push(marker); // 배열에 생성된 마커를 추가합니다
 
     return marker;
 }
@@ -206,7 +221,7 @@ function removeMarker() {
 
 // 검색결과 목록 하단에 페이지번호를 표시는 함수입니다
 function displayPagination(pagination) {
-    var paginationEl = document.getElementById('pagination'),
+    var paginationEl = document.getElementById("pagination"),
         fragment = document.createDocumentFragment(),
         i;
 
@@ -216,17 +231,17 @@ function displayPagination(pagination) {
     }
 
     for (i = 1; i <= pagination.last; i++) {
-        var el = document.createElement('a');
+        var el = document.createElement("a");
         el.href = "#";
         el.innerHTML = i;
 
         if (i === pagination.current) {
-            el.className = 'on';
+            el.className = "on";
         } else {
             el.onclick = (function (i) {
                 return function () {
                     pagination.gotoPage(i);
-                }
+                };
             })(i);
         }
 
@@ -238,7 +253,7 @@ function displayPagination(pagination) {
 // 검색결과 목록 또는 마커를 클릭했을 때 호출되는 함수입니다
 // 인포윈도우에 장소명을 표시합니다
 function displayInfowindow(marker, title) {
-    var content = '<div style="padding:5px;z-index:1;">' + title + '</div>';
+    var content = '<div style="padding:5px;z-index:1;">' + title + "</div>";
 
     infowindow.setContent(content);
     infowindow.open(map, marker);
@@ -252,32 +267,36 @@ function removeAllChildNods(el) {
 }
 
 // 우편번호 찾기 찾기 화면을 넣을 element
-var element_wrap = document.getElementById('wrap');
+var element_wrap = document.getElementById("wrap");
 
 function foldDaumPostcode() {
     // iframe을 넣은 element를 안보이게 한다.
-    element_wrap.style.display = 'none';
+    element_wrap.style.display = "none";
 }
 
 function sample3_execDaumPostcode() {
     // 현재 scroll 위치를 저장해놓는다.
-    var currentScroll = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    var currentScroll = Math.max(
+        document.body.scrollTop,
+        document.documentElement.scrollTop
+    );
     new daum.Postcode({
         oncomplete: function (data) {
             var addr = data.sido + " " + data.sigungu + " " + data.bname; // 주소 변수
+            console.log(addr);
             $(".form_location").val(addr);
             $(".address_tracking").hide();
         },
         // 우편번호 찾기 화면 크기가 조정되었을때 실행할 코드를 작성하는 부분. iframe을 넣은 element의 높이값을 조정한다.
         onresize: function (size) {
-            element_wrap.style.height = size.height + 'px';
+            element_wrap.style.height = size.height + "px";
         },
-        width: '340px',
-        height: '100%'
+        width: "340px",
+        height: "800px"
     }).embed(element_wrap);
 
     // iframe을 넣은 element를 보이게 한다.
-    element_wrap.style.display = 'block';
+    element_wrap.style.display = "block";
 }
 
 // 추가한거
@@ -287,15 +306,14 @@ $(function () {
         if (e.keyCode == 17) {
             map.setZoomable(true);
         }
-    })
+    });
     // ctrl키를 땔때
     $(document).keyup(function (e) {
         map.setZoomable(false);
-    })
+    });
 
     var timeout;
     $(".map_wrap").on("mousewheel DOMMouseScroll", function (e) {
-
         e.preventDefault();
 
         // ctrl누를때 text_p 나오게하기
@@ -312,11 +330,19 @@ $(function () {
         }
     });
 
-
+    $(".form_location").click(function () {
+        // sample3_execDaumPostcode();
+        $(".address_tracking").show();
+    });
 
     // 현재 위치에 위도, 경도를 가져오는 로직
     navigator.geolocation.getCurrentPosition(function (position) {
         console.log(position.coords.latitude, position.coords.longitude);
         my_location_find(position.coords.latitude, position.coords.longitude);
     });
+});
+
+// 주소 검색창을 닫는 버튼및 기능
+$(".cancel_address_tracking").click(function () {
+    $(".address_tracking").hide();
 });
